0dfdca45234e04624576a061db06557052a0e710
Ricardo <ricardomedeirosx@gmail.com>
2026-01-22 15:05:48 +0000

    Create and apply rotate view

--- execution/movement.c
+++ execution/movement.c
@@ -6,11 +6,24 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 21:52:01 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/01/22 14:57:59 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/01/22 15:05:00 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../Pac_Struct.h"
+
+static void	rotate_view(t_game *g, double rot)
+{
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = g->player.dir_x;
+	g->player.dir_x = g->player.dir_x * cos(rot) - g->player.dir_y * sin(rot);
+	g->player.dir_y = old_dir_x * sin(rot) + g->player.dir_y * cos(rot);
+	old_plane_x = g->player.plane_x;
+	g->player.plane_x = g->player.plane_x * cos(rot) - g->player.plane_y * sin(rot);
+	g->player.plane_y = old_plane_x * sin(rot) + g->player.plane_y * cos(rot);
+}
 
 void	apply_input(t_game *g)
 {
