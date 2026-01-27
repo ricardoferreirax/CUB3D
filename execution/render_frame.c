5a6e96cb5d4a8d9a432a20dee0b268e16a9de16c
Ricardo <ricardomedeirosx@gmail.com>
2026-01-27 20:34:00 +0000

    The render now is at good speed

--- execution/render_frame.c
+++ execution/render_frame.c
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 19:44:16 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/01/27 20:28:06 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/01/27 20:32:42 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,51 @@ static void	apply_input(t_game *g)
 		return ;
 	player_rotation_controller(g);
 	movement_controller(g);
+}
+
+static void	fill_row_fast(t_image *img, int y, int color)
+{
+	unsigned int	*row;
+	int				stride;
+	int				x;
+
+	stride = img->l_len / 4;
+	row = (unsigned int *)img->img_addr + (y * stride);
+	x = 0;
+	while (x < img->width)
+		row[x++] = (unsigned int)color;
+}
+
+void	fill_ceiling_color(t_image *img, int color, int horizon)
+{
+	int	y;
+	int	end;
+
+	if (!img || !img->img_addr)
+		return ;
+	end = img->height / 2 - horizon;
+	if (end < 0) end = 0;
+	if (end > img->height) end = img->height;
+
+	y = 0;
+	while (y < end)
+		fill_row_fast(img, y++, color);
+}
+
+void	fill_floor_color(t_image *img, int color, int horizon)
+{
+	int	y;
+	int	start;
+
+	if (!img || !img->img_addr)
+		return ;
+	start = img->height / 2 - horizon;
+	if (start < 0) start = 0;
+	if (start > img->height) start = img->height;
+
+	y = start;
+	while (y < img->height)
+		fill_row_fast(img, y++, color);
 }
 
 void	render_frame(t_game *game)
