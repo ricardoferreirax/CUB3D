e56a4047632e43437baab703c678b19528f265ab
Ricardo <ricardomedeirosx@gmail.com>
2026-01-28 17:13:13 +0000

    Validate map characters

--- parsing/map_validate_chars.c
+++ parsing/map_validate_chars.c
@@ -6,9 +6,41 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 11:53:00 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/01/28 17:01:43 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/01/28 17:12:54 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Pac_Struct.h"
 
+static int	is_valid_map_char(char c)
+{
+	if (c == '\n')
+		return (1);
+	if (c == WALL || c == OPEN_SPACE || c == FLOOR || c == VOID)
+		return (1);
+	if (c == WRAP_PORTS || c == PLAYER || c == ENERGIZER || c == GHOST_SPAWN)
+		return (1);
+	return (0);
+}
+
+void	validate_map_chars(t_game *g)
+{
+	int	y;
+	int	x;
+
+	if (!g || !g->map.grid)
+		exit_game(EXIT_MAP, g);
+
+	y = 0;
+	while (g->map.grid[y])
+	{
+		x = 0;
+		while (g->map.grid[y][x])
+		{
+			if (!is_valid_map_char(g->map.grid[y][x]))
+				exit_game(EXIT_MAP, g);
+			x++;
+		}
+		y++;
+	}
+}
