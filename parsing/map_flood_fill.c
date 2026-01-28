1f2536ca07ee9ee74c81f5ef8f4b3aa9e60efe2e
Ricardo <ricardomedeirosx@gmail.com>
2026-01-28 17:12:38 +0000

    Create map read file function

--- parsing/map_flood_fill.c
+++ parsing/map_flood_fill.c
@@ -6,84 +6,82 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/28 12:15:38 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/01/28 17:05:42 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/01/28 17:11:38 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Pac_Struct.h"
 
-static void	flood_fill(t_game *g, int y, int x, char **vis)
+static void	flood_fill(t_game *g, int y, int x, char **visited)
 {
-	char	t;
+	char	tile;
 
 	if (y < 0 || y >= g->map.height)
 		exit_game(EXIT_MAP, g);
 	if (x < 0)
 	{
-		if (map_tile(g, y, 0) != WRAP_PORTS || map_tile(g, y, g->map.width - 1) != WRAP_PORTS)
+		if (map_tile(g, y, 0) != WRAP_PORTS
+			|| map_tile(g, y, g->map.width - 1) != WRAP_PORTS)
 			exit_game(EXIT_MAP, g);
 		x = g->map.width - 1;
 	}
 	else if (x >= g->map.width)
 	{
-		if (map_tile(g, y, 0) != WRAP_PORTS || map_tile(g, y, g->map.width - 1) != WRAP_PORTS)
+		if (map_tile(g, y, 0) != WRAP_PORTS
+			|| map_tile(g, y, g->map.width - 1) != WRAP_PORTS)
 			exit_game(EXIT_MAP, g);
 		x = 0;
 	}
-	if (vis[y][x])
+	if (visited[y][x])
 		return ;
-	t = map_tile(g, y, x);
-	if (t == VOID)
+	tile = map_tile(g, y, x);
+	if (tile == VOID)
 		exit_game(EXIT_MAP, g);
-	if (t == WALL)
+	if (tile == WALL)
 		return ;
-	vis[y][x] = 1;
-	flood_fill(g, y + 1, x, vis);
-	flood_fill(g, y - 1, x, vis);
-	flood_fill(g, y, x + 1, vis);
-	flood_fill(g, y, x - 1, vis);
+	visited[y][x] = 1;
+	flood_fill(g, y + 1, x, visited);
+	flood_fill(g, y - 1, x, visited);
+	flood_fill(g, y, x + 1, visited);
+	flood_fill(g, y, x - 1, visited);
 }
 
-static char	**alloc_vis(int h, int w)
+static char	**alloc_visited_grid(int h, int w)
 {
-	char	**v;
+	char	**grid;
 	int		i;
 
-	v = ft_calloc(h + 1, sizeof(char *));
-	if (!v)
+	grid = ft_calloc(h + 1, sizeof(char *));
+	if (!grid)
 		return (NULL);
 	i = 0;
 	while (i < h)
 	{
-		v[i] = ft_calloc(w, sizeof(char));
-		if (!v[i])
+		grid[i] = ft_calloc(w, sizeof(char));
+		if (!grid[i])
 		{
 			while (i > 0)
-				free(v[--i]);
-			free(v);
+				free(grid[--i]);
+			free(grid);
 			return (NULL);
 		}
 		i++;
 	}
-	v[h] = NULL;
-	return (v);
+	grid[h] = NULL;
+	return (grid);
 }
 
 void	validate_map_flood_fill(t_game *g)
 {
-	char	**vis;
+	char	**visited;
 	int		px;
 	int		py;
 
-	vis = alloc_vis(g->map.height, g->map.width);
-	if (!vis)
+	visited = alloc_visited_grid(g->map.height, g->map.width);
+	if (!visited)
 		exit_game(EXIT_MALLOC, g);
-
 	px = (int)g->player.pos_x;
 	py = (int)g->player.pos_y;
-
-	flood_fill(g, py, px, vis);
-	free_tab_tab(vis);
+	flood_fill(g, py, px, visited);
+	free_tab_tab(visited);
 }
-
-
