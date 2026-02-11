ceecfbec25db407e2f612c00843128505903365d
Ricardo <ricardomedeirosx@gmail.com>
2026-02-11 20:53:14 +0000

    texture pick wall

--- parsing/texture_pick_wall.c
+++ parsing/texture_pick_wall.c
@@ -6,9 +6,34 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 18:13:52 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/02/11 20:50:42 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/02/11 20:52:56 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Pac_Struct.h"
 
+unsigned int	tex_pixel(t_image *tex, int x, int y)
+{
+	unsigned int	*data;
+	int				stride;
+
+	stride = tex->l_len / 4;
+	data = (unsigned int *)tex->img_addr;
+	return (data[y * stride + x]);
+}
+
+t_image	*texture_pick_wall(t_game *g)
+{
+	if (g->ray.hit_side == 0)
+	{
+		if (g->ray.ray_dir_x > 0)
+			return (&g->tex.we_img);
+		return (&g->tex.ea_img);
+	}
+	else
+	{
+		if (g->ray.ray_dir_y > 0)
+			return (&g->tex.no_img);
+		return (&g->tex.so_img);
+	}
+}
