663db126a48bce1b5849b946a7b3aa9a15b29320
Ricardo <ricardomedeirosx@gmail.com>
2026-02-23 23:47:10 +0000

    Improve player spawn parsing

--- srcs/player/player_spawn.c
+++ srcs/player/player_spawn.c
@@ -6,19 +6,14 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 21:11:27 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/02/23 23:30:20 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/02/23 23:41:09 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../Pac_Struct.h"
 #include "player3D.h"
 
-static int	is_spawn_char(char c)
-{
-	return (c == 'N' || c == 'S' || c == 'E' || c == 'W');
-}
-
-static void	set_dir_plane_from_char(t_game *g, char c)
+static void	set_dir_plane_ns(t_game *g, char c)
 {
 	if (c == 'N')
 	{
@@ -34,7 +29,11 @@ static void	set_dir_plane_from_char(t_game *g, char c)
 		g->player.plane_x = -0.66;
 		g->player.plane_y = 0;
 	}
-	else if (c == 'E')
+}
+
+static void	set_dir_plane_ew(t_game *g, char c)
+{
+	if (c == 'E')
 	{
 		g->player.dir_x = 1;
 		g->player.dir_y = 0;
@@ -50,7 +49,7 @@ static void	set_dir_plane_from_char(t_game *g, char c)
 	}
 }
 
-static void	validate_spawn_position(t_game *g, int x, int y)
+static void	apply_player_spawn(t_game *g, int x, int y, char dir)
 {
 	if (g->map.grid[y][x] == VOID)
 		exit_game(EXIT_MAP, g);
@@ -59,14 +58,10 @@ static void	validate_spawn_position(t_game *g, int x, int y)
 		|| map_get_tile(g, y + 1, x) == VOID
 		|| map_get_tile(g, y - 1, x) == VOID)
 		exit_game(EXIT_MAP, g);
-}
-
-static void	apply_player_spawn(t_game *g, int x, int y, char dir)
-{
-	validate_spawn_position(g, x, y);
 	g->player.pos_x = (double)x + 0.5;
 	g->player.pos_y = (double)y + 0.5;
-	set_dir_plane_from_char(g, dir);
+	set_dir_plane_ns(g, dir);
+	set_dir_plane_ew(g, dir);
 	g->map.grid[y][x] = OPEN_SPACE;
 }
 
@@ -85,7 +80,7 @@ static int	map_find_player_spawn(t_game *g)
 		while (x < g->map.width)
 		{
 			t = g->map.grid[y][x];
-			if (is_spawn_char(t))
+			if (t == 'N' || t == 'S' || t == 'E' || t == 'W')
 			{
 				count++;
 				apply_player_spawn(g, x, y, t);
