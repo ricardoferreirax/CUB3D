4f4827ba15c9e54d6c1c17ff422bc3144c8659f9
P3dr0-1702 <supermegagamerprofissional@gmail.com>
2026-02-25 11:47:43 +0000

    Finish map building function. Nedd to search for edge cases and set up Ghost pen properly

--- init/init.c
+++ init/init.c
@@ -254,15 +254,19 @@ int	find_sprite(unsigned int mask)
 		[0xB9] = 38, // Vertical Left
 		[0xBB] = 38,
 		[0xBD] = 38,
+		[0xBF] = 38,
 		[0xE6] = 40, // Vertical Right
 		[0xEE] = 40,
 		[0xE7] = 40,
+		[0xEF] = 40,
 		[0x73] = 17, // Horizontal Top
 		[0x77] = 17,
 		[0x7B] = 17,
+		[0x7F] = 17,
 		[0xDC] = 61, // Horizontal Bottom
 		[0xDE] = 61,
 		[0xDD] = 61,
+		[0xDF] = 61,
 		// Borders
 		[0xB1] = 104, // Bottom Vertical Left, Double Wall
 		[0xB8] = 126, // Top Vertical Left, Double Wall
@@ -272,14 +276,51 @@ int	find_sprite(unsigned int mask)
 		[0x72] = 84, //Top Horizontal Left, Double Wall
 		[0xD8] = 150, //Bottom Horizontal Right, Double Wall
 		[0xD4] = 151, //Bottom Horizontal Left, Doublw Wall
-		[0x90] = 148, // Bottom Left Curve
-		[0x30] = 82, // Bottom Right Curve
-		[0x60] = 85, // Top Right Curve
-		[0xC0] = 149, // Bottom Right Curve
-		[0x10] = 103, // Right Doorframe
-		[0x40] = 101, // Left Doorframe
+		// [0x90] = 148, // Bottom Left Curve
+		// [0x30] = 82, // Bottom Right Curve
+		// [0x60] = 85, // Top Right Curve
+		// [0xC0] = 149, // Bottom Right Curve
+		// [0x10] = 103, // Right Doorframe
+		// [0x40] = 101, // Left Doorframe
 	};
 	uint16_t tile = bitmask_to_tile[mask];
+	if(tile == 0)
+		return 0;
+	return tile;
+}
+
+int find_disamb_sprite(unsigned int mask)
+{
+	static const uint16_t bitmask_totile[256] = {
+	[0xFF] = 39,
+	[0x04] = 149, //Bottom Right Curve
+	[0x08] = 148, //Bottom Left Curve
+	[0x02] = 85, //Top Right Curve
+	[0x01] = 82, //top left curve
+	[0xCE] = 16, // Top LEft corner
+	[0x67] = 60, //Bottom Left corner
+	[0x3B] = 62, // Bottom Right Corner
+	[0x9D] = 18, //Top roght corner
+	[0x46] = 41, // Vertical Wall RIght
+	[0x44] = 41,
+	[0x42] = 41,
+	[0x40] = 41,
+	[0x19] = 43, //vertical Wall left
+	[0x18] = 43,
+	[0x11] = 43,
+	[0x10] = 43,
+	[0x8C] = 20, //Bottom Wall
+	[0x88] = 20,
+	[0x84] = 20,
+	[0x80] = 20,
+	[0x23] = 64, //Top Wall
+	[0x22] = 64,
+	[0x21] = 64,
+	[0x20] = 64,
+	[0xCC] = 103, //Right Door Frame
+	[0x9C] = 101, //LEft Door Frame
+	};
+	uint16_t tile = bitmask_totile[mask];
 	if(tile == 0)
 		return 0;
 	return tile;
@@ -289,8 +330,13 @@ void	flood_fill(char **map, int x, int y)
 {
 	if (x < 0 || x >= xtile(map) || y < 0
 		|| y >= ytile(map) || map[y][x] == 'F'
-		|| map[y][x] == '1' || map[y][x] == 'G' || map[x][y] == 'D')
+		|| map[y][x] == '1' || map[x][y] == 'D')
 		return ;
+	if(map[y][x] == 'G')
+	{
+		map[y][x] = 'F';
+		return;
+	}
     map[y][x] = 'F';
 	flood_fill(map, x + 1, y);
 	flood_fill(map, x - 1, y);
@@ -298,34 +344,41 @@ void	flood_fill(char **map, int x, int y)
 	flood_fill(map, x, y - 1);
 }
 
+
+
 int disambiguation(char **map, t_point coord)
 {
 	flood_fill(map, 24, 14);
-		
-	if(coord.y > 0 && map[coord.y - 1][coord.x] == 'F')
-		return(20);
-	if(coord.x < xtile(map) - 1 && map[coord.y][coord.x + 1] == 'F')
-		return(43);
-	if(coord.y < ytile(map) - 1 && map[coord.y + 1][coord.x] == 'F')
-		return(64);
-	if(coord.x > 0 && map[coord.y][coord.x - 1] == 'F')
-		return (41);
-	return 0;
+	unsigned char	mask;
+
+	mask = 0;
+	if (coord.y != 0 && map[coord.y - 1][coord.x] == 'F')
+		mask |= (1 << 7);
+	if (coord.y != ytile(map) - 1 && map[coord.y + 1][coord.x] == 'F')
+		mask |= (1 << 5);
+	if (coord.x != 0 && map[coord.y][coord.x - 1] == 'F')
+		mask |= (1 << 6);
+	if (coord.x != xtile(map) - 1 && map[coord.y][coord.x + 1] == 'F')
+		mask |= (1 << 4);
+	if (coord.y != 0)
+	{
+		if (coord.x != 0 && map[coord.y - 1][coord.x - 1] == 'F')
+			mask |= (1 << 2);
+		if (coord.x != xtile(map) - 1 && map[coord.y - 1][coord.x
+			+ 1] == 'F')
+			mask |= (1 << 3);
+	}
+	if (coord.y != ytile(map) - 1)
+	{
+		if (coord.x != 0 && map[coord.y + 1][coord.x - 1] == 'F')
+			mask |= (1 << 1);
+		if (coord.x != xtile(map) - 1 && map[coord.y + 1][coord.x
+			+ 1] == 'F')
+			mask |= (1 << 0);
+	}
+	int final = find_disamb_sprite(mask);
+	return final;
 }
-
-
-// char **copy_map(char **map)
-// {
-// 	char **dup = ft_calloc(sizeof(char *), ytile(map));
-// 	int i = 0;
-// 	while(map[i])
-// 	{
-// 		dup[i] = ft_strdup(map[i]);
-// 		i++;
-// 	}
-// 	dup[i] = NULL;
-// 	return dup;
-// }
 
 int squared_corners(int final)
 {
