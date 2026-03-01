c71bf8107dbdbebd3a6eea28968eac05e7fbdba7
Ricardo <ricardomedeirosx@gmail.com>
2026-03-01 22:23:12 +0000

    Improve map load map from cub and helper functions

--- srcs/map/map_load.c
+++ srcs/map/map_load.c
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 11:40:08 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/02/24 18:23:01 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/03/01 22:16:17 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,60 +45,59 @@ static int	find_map_end(t_game *g, char **file, int start)
 	return (last);
 }
 
-static char	**extract_map(char **file, int start, int end, t_game *g)
+static char	**extract_map(char **file_lines, int start, int end, t_game *g)
 {
 	int		i;
 	int		j;
-	int		n;
+	int		line_count;
 	char	**map;
 
-	n = end - start + 1;
-	if (n <= 0)
+	if (!file_lines || start < 0 || end < start)
 		return (NULL);
-	map = ft_calloc((size_t)n + 1, sizeof(char *));
+	line_count = end - start + 1;
+	map = ft_calloc((size_t)line_count + 1, sizeof(char *));
 	if (!map)
 		exit_game(EXIT_MALLOC, g);
-	i = start;
-	j = 0;
-	while (i <= end)
+	i = start - 1;
+	j = -1;
+	while (++i <= end)
 	{
-		map[j] = ft_strdup(file[i]);
+		map[++j] = ft_strdup(file_lines[i]);
 		if (!map[j])
 		{
+			map[j] = NULL;
 			free_tab_tab(map);
 			exit_game(EXIT_MALLOC, g);
 		}
-		i++;
-		j++;
 	}
-	map[j] = NULL;
+	map[j + 1] = NULL;
 	return (map);
 }
 
 char	**load_map_from_cub(t_game *g, const char *path)
 {
-	char	**file;
+	char	**file_lines;
 	int		start;
 	int		end;
 	char	**map;
 
-	file = map_read_file(path);
-	if (!file)
+	file_lines = map_read_file(path);
+	if (!file_lines)
 		exit_game(EXIT_MAP, g);
-	start = find_map_start(g, file);
+	start = find_map_start(g, file_lines);
 	if (start < 0)
 	{
-		free_tab_tab(file);
+		free_tab_tab(file_lines);
 		exit_game(EXIT_MAP, g);
 	}
-	end = find_map_end(g, file, start);
+	end = find_map_end(g, file_lines, start);
 	if (end < 0)
 	{
-		free_tab_tab(file);
+		free_tab_tab(file_lines);
 		exit_game(EXIT_MAP, g);
 	}
-	map = extract_map(file, start, end, g);
-	free_tab_tab(file);
+	map = extract_map(file_lines, start, end, g);
+	free_tab_tab(file_lines);
 	if (!map)
 		exit_game(EXIT_MAP, g);
 	return (map);
