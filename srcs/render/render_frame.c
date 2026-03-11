af48e47d7705d2bbc11d17abcc35c78b9a8bd1e1
Ricardo <ricardomedeirosx@gmail.com>
2026-03-11 22:37:26 +0000

    At this point, cub3d is working without errors! The ghosts move in scatter mode, we can use mouse to rotate the player view and I finished the debug raycast's minimap to control the distance of all casted-rays

--- srcs/render/render_frame.c
+++ srcs/render/render_frame.c
@@ -6,7 +6,7 @@
 /*   By: rmedeiro <rmedeiro@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 19:44:16 by rmedeiro          #+#    #+#             */
-/*   Updated: 2026/03/11 22:09:06 by rmedeiro         ###   ########.fr       */
+/*   Updated: 2026/03/11 22:34:35 by rmedeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -263,7 +263,7 @@ void	render_frame(t_game *game)
 	clear_sprite_z(game);
 	render_all_sprites(game);
 	render_into_framebuffer(game);
-	render_raycast_debug(game);
+	// render_raycast_debug(game);
 	mlx_put_image_to_window(game->mlx_ptr, game->win.win_ptr,
 		game->win.frame_buffer.img_ptr, 0, 0);
 }
